d68f35d5baf5f1bf61d4b9fa6316c26232f44776
yong <yong@asial.co.jp>
2023-06-10 13:53:47 +0900

    init

---
+++ src/components/app.vue
@@ -0,0 +1,48 @@
+<template>
+  <f7-app v-bind="f7params">
+
+  <!-- Your main view, should have "view-main" class -->
+  <f7-view main class="safe-areas" url="/"></f7-view>
+
+  </f7-app>
+</template>
+<script>
+  import { ref, onMounted } from 'vue';
+  import { f7, f7ready } from 'framework7-vue';
+
+
+  import routes from '../js/routes.js';
+  import store from '../js/store';
+
+  export default {
+    setup() {
+
+      // Framework7 Parameters
+      const f7params = {
+        name: 'framework7-v8-vue-blank', // App name
+        theme: 'auto', // Automatic theme detection
+
+
+
+
+        // App store
+        store: store,
+        // App routes
+        routes: routes,
+      };
+
+      onMounted(() => {
+        f7ready(() => {
+
+
+          // Call F7 APIs here
+        });
+      });
+
+      return {
+        f7params,
+
+      }
+    }
+  }
+</script>
---
+++ src/pages/home.vue
@@ -0,0 +1,19 @@
+<template>
+  <f7-page name="home">
+    <!-- Top Navbar -->
+    <f7-navbar large>
+      <f7-nav-title>framework7-v8-vue-blank</f7-nav-title>
+      <f7-nav-title-large>framework7-v8-vue-blank</f7-nav-title-large>
+    </f7-navbar>
+    <!-- Toolbar-->
+    <f7-toolbar bottom>
+      <f7-link>Left Link</f7-link>
+      <f7-link>Right Link</f7-link>
+    </f7-toolbar>
+    <!-- Page content-->
+    <f7-block>
+      <p>Here is your blank Framework7 app. Let's see what we have here.</p>
+    </f7-block>
+
+  </f7-page>
+</template>
